915212873887e995397a73b6145fe4da6820b216
Ryan Kroon <rykroon.tech@gmail.com>
2021-03-06 11:17:20 -0500

    added author an author email

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'taxee', 
     version='1.0',
+    author='Ryan Kroon',
+    author_email='rykroon.tech@gmail.com',
     packages=find_packages(),
 )
